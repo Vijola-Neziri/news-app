416502d583b47a5b414293a854f5969ffa246cfe
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2024-01-17 18:55:39 +0100

    added navbar and news

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import News from "./components/navbar/news/News";
 
 const App=()=>{
   return (
     <>
-    <h1>my app</h1>
+    <Navbar/>
+    <News/>
+
+   
     </>
   )
 }
