399355ebc01c5c39306d3f7fdfe457bcfa72ba8f
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2024-01-19 11:38:55 +0100

    edits for navbar and news

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+// App.js
+import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import News from "./components/navbar/news/News";
 
-const App=()=>{
+const App = () => {
+  const [category, setCategory] = useState("general");
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <>
-    <Navbar/>
-    <News/>
-
-   
+      <Navbar setCategory={setCategory} setSearchTerm={setSearchTerm} />
+      <News category={category} searchTerm={searchTerm} />
     </>
-  )
-}
-export default App
+  );
+};
+
+export default App;
